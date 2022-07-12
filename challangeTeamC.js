4969271c6d988af1539ac382b585df4e8155ce1a
irvan taufik <irvantaufik28@gmail.com>
2022-07-13 02:50:10 +0700

    selesai

--- challangeTeamC.js
+++ challangeTeamC.js
@@ -1,4 +1,4 @@
-// Soal:
+
 // Buatlah satu function dalam JavaScript yang menerima input berupa nilai minimum (min) dan nilai maksimum (max) kemudian mengembalikan array berupa bilangan ganjil diantara nilai minimum & maksimum!
 
 // Contoh:
@@ -8,15 +8,11 @@
 
 
 let myArray = [1, 3, 5, 7, 9, 10]
-// let newArr = arr.filter((array) => { return array % 2 == 1 });
-// console.log(newArr)
-
-
 const minMaxOdd = (array) => {
 
     let min = array[0];
     let max = array[0];
-    
+
     for (let i = 0; i < array.length; i++) {
         if (array[i] > min) {
             min = array[i];
@@ -24,13 +20,14 @@ const minMaxOdd = (array) => {
             max = array[i];
         }
     }
-    
-let odd = myArray.filter((array) => { return array % 2 == 1 })
-console.log(odd)
+
+    let odd = myArray.filter((array) => { return array % 2 == 1 })
+    console.log(odd)
 
     let result = ([max, min]);
     return result;
-        
+
 }
 
-console.log(minMaxOdd(myArray))
+ console.log(minMaxOdd(myArray))
+ 
