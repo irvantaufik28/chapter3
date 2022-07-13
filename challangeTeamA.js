943c5961763d841207da346abed64051c3eaa192
irvan taufik <irvantaufik28@gmail.com>
2022-07-13 10:32:11 +0700

    refactorig

--- challangeTeamA.js
+++ challangeTeamA.js
@@ -1,21 +1,19 @@
-// Soal:
-// Buatlah satu function dalam JavaScript yang menerima input berbentuk array of integer kemudian mengembalikan angka terbesar dari array input!
+// Buatlah satu function dalam JavaScript yang menerima input berbentuk array of integer kemudian mengembalikan angka terbesar dari array input!!!!
 
 // Contoh:
 // Input: [3,5,2,1]
 // Output: 5
 
-const arr = [3,5,2,12,10,]
+const arr = [3,5,2,1]
 const maxValue = (myArray) => {
-    let max =[];
-                // 3>0? true, 3>5 ? true, 5>2 ? false, 5>12? true, 12>10 ? false. hasil akhir nilai terbesar 12
-    for(let i=0; i< myArray.length; i++){
-        if(myArray[i] > max)
-        max = myArray[i]   
+    let max = [];
+    // 3>0? true, 3>5 ? true, 5>2 ? false, 5>12? true, 12>10 ? false. hasil akhir nilai terbesar 12
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] > max)
+            max = myArray[i]
     }
     return max;
 }
-
 console.log(maxValue(arr))
 
 
