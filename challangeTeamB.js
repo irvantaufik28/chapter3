ecf2982fab42a56f3e78fdb7c8346c39b032324e
irvan taufik <irvantaufik28@gmail.com>
2022-07-13 10:30:34 +0700

    refactorig

--- challangeTeamB.js
+++ challangeTeamB.js
@@ -6,17 +6,16 @@
 // Output: 1
 
 
-const arr = [3, 5, 2, 12, 1,10,]
+const arr = [3, 5, 2, 12, 1, 10,]
 const minValue = (myArray) => {
-    let min = arr[0]; 
-                        // 3<0 ? true, 3<5 ? false, 3<2? true, 2<12 ? false, 2<1 ? true, 1<10 ? false . hasil akhir 1 nilai terkecil
+    let min = arr[0];
+    // 3<0 ? true, 3<5 ? false, 3<2? true, 2<12 ? false, 2<1 ? true, 1<10 ? false . hasil akhir 1 nilai terkecil
     for (let i = 0; i < myArray.length; i++) {
         if (myArray[i] < min) {
             min = myArray[i];
         }
     }
     return min;
-
 }
 
 console.log(minValue(arr))
